d3dbacb94abb57fedf230eb6b31dc0d9f0a49f3c
Will-S <willschrick@gmail.com>
2024-11-09 05:59:28 -0800

    got the keypress function built

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ create a box/popup for if same letter is guessed again
 
 */
 
-let bg;
 
+let bg;
 function setup() {
   let gameArea = document.getElementById("drawingCanvas");
    let canvas = createCanvas(1122,787);
@@ -44,23 +44,64 @@ function setup() {
  
 function draw() {
     background(bg);
+    drawWordLines();
     hangMan();
 }
 
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-gameWord = "hello";  
+let gameWord = "hello";  
 let guessedLetters = [];   // this will store letters guessed
 let incorrectLetters = [] //this will store INCORRECT letters guessed
-let missedGuesses =  6 // head,body, arm, arm,leg, leg= 6
+const missedGuesses =  6 // head,body, arm, arm,leg, leg= 6
 
-
+startGame();
 
 function startGame (){
-    gameWordArray = gameWord.split(""); // this splits the word "Hello" into 
-    guessedLetters = gameWordArray.map(() => "_");
-    
+    gameWordArray = gameWord.split(""); // this splits the word "Hello" into array [h,e,l,l,o]
+    guessedLetters = gameWordArray.map(() => "_");  //this creates an array of 5 underscores for ['_', '_', '_', '_', '_']  
+    //this array will be used to match the letters and store them
 }
+
+
+
+
+
+function drawWordLines() {
+  const startX = 400;           // Starting X position for the lines
+  const startY = 100;           // Y position for the lines
+  const lineLength = 60;        // Length of each underscore line
+  const lineGap = 35;           // Gap between each line
+
+  for (let i = 0; i < guessedLetters.length; i++) {
+    let x = startX + i * (lineLength + lineGap); // Calculate X position for each letter line
+
+    // Draw the underscore line for each letter
+    stroke(0);                // Line color (black)
+    strokeWeight(3);          // Line thickness
+    line(x, startY, x + lineLength, startY); // Draw the line for the letter
+    
+    // Draw guessed letter if it has been guessed correctly
+    if (guessedLetters[i] !== "_") {
+      noStroke();              // Disable line stroke for text
+      fill(0);                 // Text color (black)
+      textSize(32);            // Font size for letters
+      textAlign(CENTER, CENTER);
+      text(guessedLetters[i], x + lineLength / 2, startY - 20); // Display letter above line
+    }
+  }
+}
+
+function keyPressed(){
+  const letterGuess = alphabet;
+  console.log(key)
+  if (letterGuess.includes(key.toLowerCase())) {
+    return letterGuess;
+  } else {
+    return incorrectLetters;
+  }
+}
+
 
 
 
